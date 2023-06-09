a7557b48224886c3987aabb70c111b246174e529
jcg0 <jg.awsprod@gmail.com>
2023-06-09 15:01:31 -0400

    added more functionality to the state objects and now have multiple states that handle the title, amount, and date inputs. also added a submit handler which now add functionality on form submit

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,8 +1,17 @@
+import React, { useState } from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const titleClickHandler = () => {
+    setTitle("updated");
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,6 +19,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item-price">${props.amount}</div>
       </div>
+      <button onClick={titleClickHandler}>Change Title</button>
     </Card>
   );
 };
---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,71 @@
+import { React, useState } from "react";
+
+const ExpenseForm = () => {
+  const [title, setNewTitle] = useState("");
+  const [amount, setNewAmount] = useState("");
+  const [date, setNewDate] = useState("");
+
+  const titleChangeHandler = (e) => {
+    setNewTitle(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const amountChangeHandler = (e) => {
+    setNewAmount(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const dateChangeHandler = (e) => {
+    setNewDate(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const expenseData = {
+      title: title,
+      amount: amount,
+      date: new Date(date),
+    };
+
+    console.log(expenseData);
+    setNewTitle("");
+    setNewAmount("");
+    setNewDate("");
+  };
+
+  return (
+    <form action="" onSubmit={handleSubmit}>
+      <div>
+        <div>
+          <label htmlFor="">Title</label>
+          <input type="text" value={title} onChange={titleChangeHandler} />
+        </div>
+        <div>
+          <label htmlFor="">Amount</label>
+          <input
+            type="number"
+            value={amount}
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
+        </div>
+        <div>
+          <label htmlFor="">Date</label>
+          <input
+            type="date"
+            value={date}
+            min="2023-01-01"
+            max="2030-01-01"
+            onChange={dateChangeHandler}
+          />
+        </div>
+      </div>
+      <button type="submit">Add Expense</button>
+    </form>
+  );
+};
+
+export default ExpenseForm;
