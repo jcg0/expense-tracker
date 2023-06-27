a7f6f1a0895202a2a16a9165cff50493ae686847
jcg0 <jg.awsprod@gmail.com>
2023-06-27 10:20:23 -0400

    cleaned up expenseitem component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const titleClickHandler = () => {
-    setTitle("updated");
-    console.log(title);
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -19,7 +12,6 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item-price">${props.amount}</div>
       </div>
-      <button onClick={titleClickHandler}>Change Title</button>
     </Card>
   );
 };
