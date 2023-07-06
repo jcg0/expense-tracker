091c357e93437592d40195c9da07ea6d3e677dcf
jcg0 <jg.awsprod@gmail.com>
2023-07-06 14:26:17 -0400

    added conditional rendering to the form and a button which now hides and shows the form when clicked. Updated css so whole app is uniform

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,37 @@
-import React from "react";
+import { React, useState } from "react";
 import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [editState, setEditState] = useState();
   const SaveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setEditState(false);
+  };
+
+  const editStateHandler = () => {
+    setEditState(true);
+  };
+
+  const cancelExpenseHandler = () => {
+    setEditState(false);
   };
 
   return (
-    <div>
-      <ExpenseForm onSaveExpenseData={SaveExpenseDataHandler} />
+    <div className="new-expense">
+      {!editState && (
+        <button onClick={editStateHandler}>Add new Expense</button>
+      )}
+      {editState && (
+        <ExpenseForm
+          onSaveExpenseData={SaveExpenseDataHandler}
+          cancel={cancelExpenseHandler}
+        />
+      )}
     </div>
   );
 };
